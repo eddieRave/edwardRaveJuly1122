1cd55a5aa5068c0d1f22d7c28801912c3037f808
philipper <philipper@ravebizz.com>
2022-07-12 10:06:04 -0400

    Finished the tests

--- 01-DataTypes/StringProject/String/Tester+Strings.swift
+++ 01-DataTypes/StringProject/String/Tester+Strings.swift
@@ -11,25 +11,42 @@ extension Tester {
     /// Creating a git conflict
     /// Adding comment
     ///
+    ///
+    /// DONE
     /// Using print command
     func printWord(word: String) {
+        print(word)
     }
     
+    // DONE
     /// input "hello" should print "HELLO"
     func printUpperCase(word: String) {
+        print(word.uppercased())
     }
     
+    // DONE
     /// Using for loop, print each char in word
     func printLetters(word: String) {
-
+        for letter in word {
+            print(letter)
+        }
     }
     
+    // DONE
     /// Using for-in-enumerated(), print letters at every even-index
     func printEvenIndexEnumerated(word: String) {
+        for(n,c) in word.enumerated() {
+            if (n % 2 == 0) {
+                print("\(c)")
+            }
+        }
     }
     
     /// Using for-in-stride and string.index, print letters at every even-index
     func printEvenIndexStride(word: String) {
+        for i in stride(from: 0, through: word.count - 1, by: 2) {
+            print(word[word.index(word.startIndex,offsetBy: i)])
+        }
     }
     
     /**
@@ -40,12 +57,42 @@ extension Tester {
      2) IF word starts with a vowel, add "yay" to the word
      one -> oneyay
      */
+    // DONE
     func printPigLatin(word: String) {
+        // make all letters of word lowercased
+        var newWord = word.lowercased()
+        
+        // get the first letter of the word
+        let firstLetter = newWord.first!
+        
+        // Remove the first letter of new word for use if first letter was not a vowel
+        newWord.removeFirst()
+
+        // Make a character array
+        let vowels = "aeiou"
+        
+        // Determine if the first letter is a vowel or not
+        if vowels.contains(firstLetter) {
+                print(word + "yay")
+        } else {
+            print(newWord + String(firstLetter) + "ay")
+        }
+        
     }
     
     /// Using loop and string.index, returns true if input is a palindrome
     /// Do not use .reversed()
     func isPalindrome(word: String) -> Bool {
-        return false
+        let characters = Array(word)
+        var currentIndex = 0
+        
+        while currentIndex < characters.count / 2 {
+            if characters[currentIndex] != characters[characters.count - currentIndex - 1] {
+                return false
+            }
+            currentIndex += 1
+        }
+        
+        return true
     }
 }
