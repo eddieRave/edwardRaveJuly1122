b177a1927db77b3e57a28456e85c551768ad62c4
kdm326 <108142704+kdm326@users.noreply.github.com>
2022-07-13 10:13:17 -0400

    03complete

--- 03-optionals_&_dictionaries/SetDictionaryProject/SetDictionaryProject/Tester+Optionals.swift
+++ 03-optionals_&_dictionaries/SetDictionaryProject/SetDictionaryProject/Tester+Optionals.swift
@@ -12,69 +12,76 @@ extension Tester {
     /// Unwrap using optional binding: if let
     /// else return 0
     func unwrap(optional: Int?) -> Int {
-        return 0
+        if let int = optional { return int }
+        else { return 0 }
     }
     
     /// Unwrap using optional binding: guard let
     /// else return ""
     func unwrap(optional: String?) -> String {
-        return ""
+        guard let string = optional
+        else { return "" }
+        return string
     }
     
     /// Unwrap using nil coalescing
     /// takes optional array, return first value?
     func unwrap(optional: [Int]?) -> Int {
         
-        return 0
+        return optional?.first ?? 0
     }
     
     /// Create a url from a string address, avoid force unwrapping
     /// else return the main bundle url
     func createURL(from address: String?) -> URL {
-        
-        
-        return Bundle.main.bundleURL
+        URL(string: address ?? "") ?? Bundle.main.bundleURL
     }
     
     
     /// Input array of [Int?], unwrap each Int? and return the an array of Int
     /// Input [Int?] = [1, nil, nil, 2] -> [1, 2]
     func unwrapElementsInArray(array: [Int?]) -> [Int] {
-        
-        
-        return []
+        var ints: [Int] = []
+        for int in array {
+            if let goodInt = int { ints.append(goodInt) }
+        }
+        return ints
     }
     
     /// Unwrap using nil coalescing
     /// takes optional dictionary, return value at key?
     func unwrap(optional: [Int: Int]?, key: Int?) -> Int {
         
-        return 0
+        if let int = optional?[key ?? 0] { return int }
+        else { return 0 }
     }
     
     /// Unwrap using optional chaining and optional binding
     /// takes nested optional array, return first value?
     func unwrap(optional: [[Int]?]?) -> Int {
-        
+        if let int = optional?.first??.first { return int }
         return 0
     }
     
     /// Unwrap using implicitly unwrap
     func implicitUnwrap(optional: Int?) -> Int {
-        
-        return 0
+        var int: Int!
+        int = optional
+        return int
     }
     
     /// Cast string into non-optional Int
     func castStringIntoInt(string: String) -> Int {
         
-        return 0
+        if let int = Int(string) { return int }
+        
+        else { return 0 }
     }
     
     /// Cast an array of string into an array of Int, remove any nil values
     /// ["1", "5", "apple", "2"] --> [1, 5, 2]
     func castArray(from strArr: [String]) -> [Int] {
     
-        return []
+        return unwrapElementsInArray(array: strArr.map({ Int($0) }))
     }
 }
--- 03-optionals_&_dictionaries/SetDictionaryProject/SetDictionaryProject/Tester+SetDict.swift
+++ 03-optionals_&_dictionaries/SetDictionaryProject/SetDictionaryProject/Tester+SetDict.swift
@@ -10,30 +10,50 @@ import Foundation
 struct Tester {
     /// Remove an array with the duplicated values removed
     func removeDups(nums: [Int]) -> [Int] {
-        
-        return []
+        Array(Set(nums))
     }
     
     /// Find the nums that exist in both arrays
     func findCommonNums(from firstNums: [Int], and secondNums: [Int]) -> [Int] {
+        var commonNums: [Int] = []
         
-        return []
+        firstNums.forEach{
+            if secondNums.contains($0) {
+                commonNums.append($0)
+            }
+        }
+        return commonNums
     }
     
     /// first: "hello", second: "bye" -> ["e"]
     func findMatchingLetters(firstWord: String, secondWord: String) -> [Character] {
-        return []
+        var commonLetters: [Character] = []
+        
+        firstWord.forEach{
+            if secondWord.contains($0) {
+                commonLetters.append($0)
+            }
+        }
+        return commonLetters
     }
     
     
     /// Create a dictionary of the counts of the letters, ignoring spaces
     func createCounter(string: String) -> [Character: Int] {
-        return [:]
+        var counter: [Character: Int] = [:]
+    
+        string.forEach {
+            if $0 != " " {
+                counter[$0] = (counter[$0] ?? 0) + 1
+            }
+        }
+        
+        return counter
     }
     
     /// Find most frequent letter in string: "hello there" -> "e"
     func mostFrequentLetter(string: String) -> Character {
-        return "."
+        createCounter(string: string).max(by: { $0.value < $1.value })!.key
     }
     
     /// Given a string of numbers: "121" and a sentence "fire water fire"
@@ -42,15 +62,18 @@ struct Tester {
     /// "1122" and a sentence "fire water fire water" (returns false)
     /// "11221" and a sentence "fire fire water water fire" (returns true)
     func translateNums(sequence: String, sentence: String) -> Bool {
-
-        return false
+        sentence.components(separatedBy: " ").map({ $0 == "fire" ? "1" : "2" }) == sequence.map({ $0 })
     }
     
     /// Find number of pairs that SUMS up to 0
     /// O(n) time: don't use nested for loops
     func findPairsOfOpposites(nums: [Int]) -> Int {
+        var count = 0
+        for (index, num) in nums.enumerated() {
+            if nums.suffix(nums.count - index - 1).contains(where: { $0 + num == 0 }) { count += 1 }
+        }
         
-        return 0
+        return count
     }
     
     /**
@@ -60,8 +83,7 @@ struct Tester {
      [1, 4, 8, 1] -> [1, 2, 3, 1]
      */
     func reduceDistanceKeepPriority(array: [Int]) -> [Int] {
-        
-        return []
+        array.map { Set(array).sorted().firstIndex(of: $0)! + 1 }
     }
     
     
@@ -76,12 +98,38 @@ struct Tester {
      M = 1000
      
      IV = 4, IX = 9
-     XL = 40, XC = 400
+     XL = 40, XC = 90
      CD = 400, CM = 900
      */
     
     func romanToInt(roman: String) -> Int {
-        return 0
+        var prevChar: Character = " "
+        var int = 0
+        
+        roman.forEach {
+            switch $0 {
+            case "I":
+                int += 1
+            case "V":
+                int += prevChar != "I" ? 5 : 3
+            case "X":
+                int += prevChar != "I" ? 10 : 8
+            case "L":
+                int += prevChar != "X" ? 50 : 30
+            case "C":
+                int += prevChar != "X" ? 100 : 80
+            case "D":
+                int += prevChar != "C" ? 500 : 300
+            case "M":
+                int += prevChar != "C" ? 1000 : 800
+            default:
+                break
+            }
+            
+            
+            prevChar = $0
+        }
+        return int
     }
     
 }
