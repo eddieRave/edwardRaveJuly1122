9ac5f76d1819f0ebbc26b054e989b01d3a9638c8
Joel M <HQ1Ricoh>
2022-07-15 12:14:30 -0400

    Assignment 6 complete

---
+++ 06-AutoLayout/MyAutoLayout/MyAutoLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyAutoLayout
+//
+//  Created by Joel Maldonado on 7/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
