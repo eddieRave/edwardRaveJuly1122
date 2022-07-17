52782202ace41bc98b6aa5f6c4c0b1fd64f29d40
WillC <willc@WillCs-Air.hsd1.pa.comcast.net>
2022-07-17 12:38:35 -0400

    done

--- 07-Enums/enumsAssigment/enums/TemperatureScale.swift
+++ 07-Enums/enumsAssigment/enums/TemperatureScale.swift
@@ -13,13 +13,52 @@ import Foundation
  3) Implement tempInFahren property
  */
 
-enum TemperatureScale {
+enum TemperatureScale: TempConvertable {
+    func getTempInFahrenheit() -> Int {
+        switch self {
+        case .fahrenheit(let temp):
+            return temp
+        case .celcius(let temp):
+            return Int(32.0 + Float(9.0 / 5.0 * Float(temp)) )
+        case .kelvin(let temp):
+            return (temp - 273) * 9/5 + 32
+        }
+    }
+    
+    var tempInFahren: Int {
+        return self.getTempInFahrenheit()
+    }
+    
     case fahrenheit(Int)
     case celcius(Int)
     case kelvin(Int)
+    // implementation of property
 }
 
 protocol TempConvertable {
     func getTempInFahrenheit() -> Int
     var tempInFahren: Int { get }
 }
+
+//enum TemperatureScale : TempConvertable {
+//    var tempInFahren: Int {
+//        get {
+//            switch self {
+//            case .celcius:
+//                return 1
+//            case .kelvin:
+//                return 2
+//            case .fahrenheit:
+//                return -1
+//            }
+//
+//        }
+//    }
+//    func getTempInFahrenheit()->Int {
+//        return 212
+//    }
+//
+//
+//}
+
+
