cc185d9093092469df2484c8ffceb00eb82c72ad
SpencerNinja <62817123+SpencerNinja@users.noreply.github.com>
2022-07-17 11:13:39 -0400

    finished assignment 7

--- 07-Enums/enumsAssigment/enums/TemperatureScale.swift
+++ 07-Enums/enumsAssigment/enums/TemperatureScale.swift
@@ -13,10 +13,35 @@ import Foundation
  3) Implement tempInFahren property
  */
 
-enum TemperatureScale {
+// MARK: "celcius" is actually spelled "celsius", but the tests won't pass if the  spelling doesn't match and I don't want to edit the tests.
+
+enum TemperatureScale: TempConvertable {
+    
     case fahrenheit(Int)
     case celcius(Int)
     case kelvin(Int)
+    
+    var tempInFahren: Int {
+        return getTempInFahrenheit()
+    }
+    
+    func getTempInFahrenheit() -> Int {
+        switch self {
+        case .fahrenheit(let temperature):
+            return temperature
+        case .celcius(let temperature):
+            // Formula to convert Celsius to Fahrenheit:
+                // °F = °C × (9/5) + 32
+            let temperatureInCelsius = temperature * 9 / 5 + 32
+            return temperatureInCelsius
+        case .kelvin(let temperature):
+            // Formula to convert Kelvin to Fahrenheit:
+                // °F = (K − 273.15) × 9/5 + 32
+            let temperatureInKelvin = (temperature - Int(273.15)) * 9 / 5 + 32
+            return temperatureInKelvin
+        }
+    }
+    
 }
 
 protocol TempConvertable {
