db4c531aead564d5ebb64dbde664bf9035079497
maherDamouni <damounim1.gmail.com>
2022-07-17 12:50:08 -0400

    completed up to 4a

---
+++ 06-AutoLayout/UI-assignment-06/UI-assignment-06/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  UI-assignment-06
+//
+//  Created by Maher Damouni on 7/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let label = UILabel()
+    @IBOutlet weak var leftButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func middleButton(_ sender: UIButton, forEvent event: UIEvent) {
+        
+        leftButton.titleLabel = UILabel.
+    }
+    
+}
+
