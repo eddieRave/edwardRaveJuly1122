ec3b0dba49de7dd865d7241dca4d31cf3474fdd9
SpencerNinja <62817123+SpencerNinja@users.noreply.github.com>
2022-07-17 11:20:38 -0400

    renamed more variables to make more sense

--- 07-Enums/enumsAssigment/enums/TemperatureScale.swift
+++ 07-Enums/enumsAssigment/enums/TemperatureScale.swift
@@ -29,15 +29,15 @@ enum TemperatureScale: TempConvertable {
         switch self {
         case .fahrenheit(let temperature):
             return temperature
-        case .celcius(let temperature):
+        case .celcius(let temperatureInC):
             // Formula to convert Celsius to Fahrenheit:
                 // °F = °C × (9/5) + 32
-            let temperatureInFahrenheit = temperature * 9 / 5 + 32
+            let temperatureInFahrenheit = temperatureInC * 9 / 5 + 32
             return temperatureInFahrenheit
-        case .kelvin(let temperature):
+        case .kelvin(let temperatureInK):
             // Formula to convert Kelvin to Fahrenheit:
                 // °F = (K − 273.15) × 9/5 + 32
-            let temperatureInFahrenheit = (temperature - Int(273.15)) * 9 / 5 + 32
+            let temperatureInFahrenheit = (temperatureInK - Int(273.15)) * 9 / 5 + 32
             return temperatureInFahrenheit
         }
     }
