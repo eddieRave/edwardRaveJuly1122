b10075b00d2f9d684ef4b32736d00ed2bc05a43f
Rave Bizz 63 <ravebizzz63@Raves-MacBook-Air.local>
2022-07-18 10:04:19 -0400

    Finished 06

---
+++ 06-AutoLayout/Assignment 6/Assignment 6/FirstViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.swift
+//  Assignment 6
+//
+//  Created by Rave Bizz 63 on 7/17/22.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ 06-AutoLayout/Assignment 6/Assignment 6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment 6
+//
+//  Created by Rave Bizz 63 on 7/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
