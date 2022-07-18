5d5a075bee73a0d16405d63a7557654956a69c46
WillC <willc@ravebizz67s-air.mynetworksettings.com>
2022-07-18 13:48:56 -0400

    done

--- 08-Networking/apiCalls/apiCalls/Models.swift
+++ 08-Networking/apiCalls/apiCalls/Models.swift
@@ -8,18 +8,30 @@
 import Foundation
 
 struct Joke: Decodable {
-    
+    let value: String?
 }
 
 struct Drink: Decodable {
-    
+    let drinks: [StrDrink]?
+    struct StrDrink: Decodable{
+        let strDrink: String?
+    }
 }
 
 struct Episode: Decodable {
-    
+    let name: String?
 }
-
+//"results": [
+//    {
+//      "artistName": "Bad Bunny",
 struct Music: Decodable {
+    let feed : Feed
+    struct Feed: Decodable{
+        let results: [Album]?
+        struct Album: Decodable{
+            let artistName: String?
+        }
+    }
     
 }
 
--- 08-Networking/apiCalls/apiCalls/NetworkService.swift
+++ 08-Networking/apiCalls/apiCalls/NetworkService.swift
@@ -16,15 +16,71 @@ let musicStr = "https://rss.applemarketingtools.com/api/v2/us/music/most-played/
 class NetworkService {
 
     func fetchJoke(completion: @escaping (Joke?) -> Void) {
-        
+        guard let url = URL(string: jokesStr) else{
+               return
+           }
+        URLSession.shared.dataTask(with: url){
+            data, response, error in
+            if let data = data {
+                do {
+                    let decodedData  = try JSONDecoder().decode(Joke.self, from: data)
+                    completion(decodedData)
+                    print(decodedData)
+                } catch  {
+                    print(error)
+                }
+            }
+        }.resume()
     }
     func fetchDrink(completion: @escaping (Drink?) -> Void) {
-        
+        guard let url = URL(string: drinksStr) else{
+               return
+           }
+        URLSession.shared.dataTask(with: url){
+            data, response, error in
+            if let data = data {
+                do {
+                    let decodedData  = try JSONDecoder().decode(Drink.self, from: data)
+                    completion(decodedData)
+                    print(decodedData)
+                } catch  {
+                    print(error)
+                }
+            }
+        }.resume()
     }
     func fetchEpisode(completion: @escaping (Episode?) -> Void) {
-        
+        guard let url = URL(string: tvShowsStr) else{
+               return
+           }
+        URLSession.shared.dataTask(with: url){
+            data, response, error in
+            if let data = data {
+                do {
+                    let decodedData  = try JSONDecoder().decode([Episode].self, from: data)
+                    completion(decodedData.first)
+                } catch  {
+                    print(error)
+                }
+            }
+        }.resume()
     }
+
     func fetchMusic(completion: @escaping (Music?) -> Void) {
-        
+        guard let url = URL(string: musicStr) else{
+               return
+           }
+        URLSession.shared.dataTask(with: url){
+            data, response, error in
+            if let data = data {
+                do {
+                    let decodedData  = try JSONDecoder().decode(Music.self, from: data)
+                    completion(decodedData)
+                    print(decodedData)
+                } catch  {
+                    print(error)
+                }
+            }
+        }.resume()
     }
 }
