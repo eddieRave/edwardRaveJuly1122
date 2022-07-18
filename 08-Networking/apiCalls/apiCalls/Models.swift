8c82fe52304a25f8ecc8688beba9059766354ac4
SpencerNinja <62817123+SpencerNinja@users.noreply.github.com>
2022-07-18 11:45:26 -0400

    initial changes

--- 08-Networking/apiCalls/apiCalls/Models.swift
+++ 08-Networking/apiCalls/apiCalls/Models.swift
@@ -8,8 +8,19 @@
 import Foundation
 
 struct Joke: Decodable {
-    
+    let value: String?
 }
+/*
+ {
+    "categories":[],
+    "created_at":"2020-01-05 13:42:30.177068",
+    "icon_url":"https://assets.chucknorris.host/img/avatar/chuck-norris.png",
+    "id":"-vZWD2GgTjaf1PO7F2kgng",
+    "updated_at":"2020-01-05 13:42:30.177068",
+    "url":"https://api.chucknorris.io/jokes/-vZWD2GgTjaf1PO7F2kgng",
+    "value":"Chuck Norris doesn't drink water he drinks sulfuric acid with a hint of lemon"
+ }
+ */
 
 struct Drink: Decodable {
     
--- 08-Networking/apiCalls/apiCalls/NetworkService.swift
+++ 08-Networking/apiCalls/apiCalls/NetworkService.swift
@@ -14,17 +14,61 @@ let musicStr = "https://rss.applemarketingtools.com/api/v2/us/music/most-played/
 
 /// Only pass the first items for testing purposes
 class NetworkService {
-
+    static let shared = NetworkService()
+    private init() {}
+    
     func fetchJoke(completion: @escaping (Joke?) -> Void) {
-        
+        guard let url = URL(string: jokesStr) else{
+            return
+        }
+        URLSession.shared.dataTask(with: url){
+            data, response, error in
+            if let data = data {
+                do {
+                    let decodedData  = try JSONDecoder().decode(Joke.self, from: data)
+                    completion(decodedData)
+                } catch  {
+                    print(error)
+                }
+            }
+        }.resume()
     }
+    
     func fetchDrink(completion: @escaping (Drink?) -> Void) {
         
     }
+    
     func fetchEpisode(completion: @escaping (Episode?) -> Void) {
         
     }
+    
     func fetchMusic(completion: @escaping (Music?) -> Void) {
         
     }
+    
 }
+
+/*
+ static let shared = APIManager()
+ private init(){}
+ 
+ let url = "https://api.jokes.one/jod"
+ 
+ func fetchData(completion: @escaping (JokeModel) -> Void){
+     guard let url = URL(string: url) else{
+         return
+     }
+     URLSession.shared.dataTask(with: url){
+         data, response, error in
+         if let data = data {
+             do {
+                 let decodedData  = try JSONDecoder().decode(JokeModel.self, from: data)
+                 completion(decodedData)
+             } catch  {
+                 print(error)
+             }
+         }
+     }.resume()
+     print("line 31")
+ }
+ */
