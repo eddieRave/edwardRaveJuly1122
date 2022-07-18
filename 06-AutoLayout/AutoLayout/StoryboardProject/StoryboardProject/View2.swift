e6e66e7fbdd8410b713e48bac3263f76fd693dab
WillC <willc@WillCs-Air.hsd1.pa.comcast.net>
2022-07-17 21:35:19 -0400

    done

---
+++ 06-AutoLayout/AutoLayout/StoryboardProject/StoryboardProject/View2.swift
@@ -0,0 +1,20 @@
+//
+//  View2.swift
+//  StoryboardProject
+//
+//  Created by WillC on 7/17/22.
+//
+
+import SwiftUI
+
+struct View2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct View2_Previews: PreviewProvider {
+    static var previews: some View {
+        View2()
+    }
+}
