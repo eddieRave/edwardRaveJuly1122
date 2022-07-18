c998eafae48f917b25f5489f2144fd0ff9b360a6
Rave Bizz 63 <ravebizzz63@Raves-MacBook-Air.local>
2022-07-18 16:07:08 -0400

    Finished 08

--- 08-Networking/apiCalls/apiCalls/Models.swift
+++ 08-Networking/apiCalls/apiCalls/Models.swift
@@ -8,18 +8,28 @@
 import Foundation
 
 struct Joke: Decodable {
-    
+    let value: String?
 }
 
 struct Drink: Decodable {
-    
+    let drinks: [Drink]
+    struct Drink: Decodable {
+        let strDrink: String?
+    }
 }
 
 struct Episode: Decodable {
-    
+    let name: String?
 }
 
 struct Music: Decodable {
+    let feed: Feed
     
+    struct Feed: Decodable {
+        let results: [Result]
+        
+        struct Result: Decodable {
+            let artistName: String
+        }
 }
-
+}
