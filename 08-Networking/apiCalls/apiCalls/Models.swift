6e7b38a4b2c82d24c199160c2ec86aa8c29ab809
Joel M <HQ1Ricoh>
2022-07-18 12:47:37 -0400

    Assignment 8 complete

--- 08-Networking/apiCalls/apiCalls/Models.swift
+++ 08-Networking/apiCalls/apiCalls/Models.swift
@@ -8,18 +8,34 @@
 import Foundation
 
 struct Joke: Decodable {
-    
+    let value: String?
 }
 
 struct Drink: Decodable {
-    
+    let drinks: [OneDrink]?
+}
+
+struct OneDrink: Decodable {
+    let strDrink: String?
 }
 
 struct Episode: Decodable {
-    
+    let name: String?
+}
+
+struct OneEpisode: Decodable {
+    let name: String?
 }
 
 struct Music: Decodable {
-    
+    let feed: Feed?
+}
+
+struct Feed: Decodable {
+    let results: [Album]?
+}
+
+struct Album: Decodable {
+    let artistName: String?
 }
 
--- 08-Networking/apiCalls/apiCalls/ViewController.swift
+++ 08-Networking/apiCalls/apiCalls/ViewController.swift
@@ -19,22 +19,49 @@ class ViewController: UIViewController {
     /// 2. Parse the JSON
     /// 3. Update firstLabel with the first item only
     @IBAction func jokesPressed(_ sender: UIButton) {
-        firstLabel.text = "Testing Joke button"
-        
+        networkService.fetchJoke(){
+            data in
+            if let joke = data?.value {
+                DispatchQueue.main.async {
+                    self.firstLabel.text = joke
+                }
+            }
+        }
     }
     
     /// Display the first drink's name
     @IBAction func drinksPressed(_ sender: UIButton) {
-        firstLabel.text = "Testing Drinks button"
+        networkService.fetchDrink(){
+            data in
+            if let drink = data?.drinks?.first?.strDrink {
+                DispatchQueue.main.async {
+                    self.firstLabel.text = drink
+                }
+            }
+        }
     }
     
     /// Show the name of the first episode
     @IBAction func tvShowsPressed(_ sender: UIButton) {
-        firstLabel.text = "Testing TVShows button"
+        networkService.fetchEpisode(){
+            data in
+            if let episode = data?.name {
+                DispatchQueue.main.async {
+                    self.firstLabel.text = episode
+                }
+            }
+        }
     }
     /// Show the artistName from the first result of the response
     @IBAction func musicPressed(_ sender: UIButton) {
-        firstLabel.text = "Testing music button"
+        networkService.fetchMusic(){
+            data in
+            if let artistName = data?.feed?.results?.first?.artistName {
+                DispatchQueue.main.async {
+                    self.firstLabel.text = artistName
+                }
+            }
+        }
     }
     
     override func viewDidLoad() {
