e523d1f646faa5bb2dae31c6c54450c12b6c489e
maherDamouni <damounim1.gmail.com>
2022-08-04 08:40:47 -0400

    deleted .git

---
+++ 12-TableView/ToDoListPersist/ToDoListPersist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoListPersist
+//
+//  Created by Maher Damouni on 8/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
